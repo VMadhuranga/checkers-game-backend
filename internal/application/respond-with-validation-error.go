ac1d92ea7394037e4af4bb6f4ce51c3e252667b7
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-27 07:47:55 +0530

    Define respondWithValidationError function

---
+++ internal/application/respond-with-validation-error.go
@@ -0,0 +1,9 @@
+package application
+
+import (
+	"net/http"
+)
+
+func respondWithValidationError(w http.ResponseWriter, statusCode int, errors interface{}) {
+	respondWithJson(w, statusCode, map[string]interface{}{"errors": errors})
+}
