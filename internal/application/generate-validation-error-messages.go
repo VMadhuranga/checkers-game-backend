3f5f6045a59f6762648e3d9030d8557a688e1547
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-27 07:46:02 +0530

    Define generateValidationErrorMessages and appendValidationErrorMessage functions

---
+++ internal/application/generate-validation-error-messages.go
@@ -0,0 +1,24 @@
+package application
+
+import "github.com/go-playground/validator/v10"
+
+func appendValidationErrorMessage(field []string, err validator.FieldError) []string {
+	return append(field, validationErrorMessages[validationError{err.StructField(), err.ActualTag()}])
+}
+
+func generateValidationErrorMessages(errors validator.ValidationErrors) validationErrorMessagesResponse {
+	messages := validationErrorMessagesResponse{}
+
+	for _, err := range errors {
+		switch err.StructField() {
+		case "Username":
+			messages.Username = appendValidationErrorMessage(messages.Username, err)
+		case "Password":
+			messages.Password = appendValidationErrorMessage(messages.Password, err)
+		case "ConfirmPassword":
+			messages.ConfirmPassword = appendValidationErrorMessage(messages.ConfirmPassword, err)
+		}
+	}
+
+	return messages
+}
