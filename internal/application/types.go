f62ca55acc1a009fe521eaf23a411623a525bbc4
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-27 07:44:40 +0530

    Define types

---
+++ internal/application/types.go
@@ -0,0 +1,27 @@
+package application
+
+import (
+	"github.com/VMadhuranga/checkers-game-backend/internal/database"
+	"github.com/go-playground/validator/v10"
+)
+
+type Application struct {
+	Queries  *database.Queries
+	Validate *validator.Validate
+}
+
+type createUserPayload struct {
+	Username        string `json:"username,omitempty" validate:"required,alpha"`
+	Password        string `json:"password,omitempty" validate:"required,alphanum,min=5"`
+	ConfirmPassword string `json:"confirm_password,omitempty" validate:"eqfield=Password"`
+}
+
+type validationError struct {
+	field, tag string
+}
+
+type validationErrorMessagesResponse struct {
+	Username        []string `json:"username,omitempty"`
+	Password        []string `json:"password,omitempty"`
+	ConfirmPassword []string `json:"confirm_password,omitempty"`
+}
