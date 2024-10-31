79a616599886273d6e801c23838f1f65a6b37cf0
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-31 07:41:27 +0530

    Setup cors

--- internal/application/initialize-router.go
+++ internal/application/initialize-router.go
@@ -1,14 +1,25 @@
 package application
 
 import (
+	"os"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func InitializeRouter(app Application) *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{os.Getenv("FRONTEND_BASE_URL")},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	v1Router := chi.NewRouter()
 
