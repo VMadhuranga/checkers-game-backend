7efb5104d1a8d3fb72938f602d314ab37f031179
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-06 08:36:34 +0530

    Define userResponse struct

--- internal/application/types.go
+++ internal/application/types.go
@@ -3,6 +3,7 @@ package application
 import (
 	"github.com/VMadhuranga/checkers-game-backend/internal/database"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 )
 
 type application struct {
@@ -30,4 +31,9 @@ type validationErrorMessagesResponse struct {
 	Username        []string `json:"username,omitempty"`
 	Password        []string `json:"password,omitempty"`
 	ConfirmPassword []string `json:"confirm_password,omitempty"`
+}
+
+type userResponse struct {
+	Id       uuid.UUID `json:"id,omitempty"`
+	Username string    `json:"username,omitempty"`
 }
