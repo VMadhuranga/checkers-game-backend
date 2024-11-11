990ae94c433a2c37b0ff3d894758f5d6f644913e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-11 15:57:38 +0530

    Update InitializeApplication function to return pointer to application

--- internal/application/initialize-application.go
+++ internal/application/initialize-application.go
@@ -9,8 +9,8 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-func InitializeApplication(db *sql.DB) application {
-	return application{
+func InitializeApplication(db *sql.DB) *application {
+	return &application{
 		queries:             database.New(db),
 		validate:            validator.New(validator.WithRequiredStructEnabled()),
 		accessTokenSecret:   os.Getenv("ACCESS_TOKEN_SECRET"),
