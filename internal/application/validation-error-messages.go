a4e8baf1bb2e73ebe191b24063aeafa6cddf913d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-23 20:14:32 +0530

    Add new validation messages for OldPassword, NewPassword, ConfirmNewPassword

--- internal/application/validation-error-messages.go
+++ internal/application/validation-error-messages.go
@@ -33,4 +33,28 @@ var validationErrorMessages = map[validationError]string{
 		"NewUsername",
 		"alpha",
 	}: "New user name must contain letters only",
+	{
+		"OldPassword",
+		"required",
+	}: "Old password is required",
+	{
+		"OldPassword",
+		"alphanum",
+	}: "Old password must contain letters and numbers only",
+	{
+		"NewPassword",
+		"required",
+	}: "New password is required",
+	{
+		"NewPassword",
+		"alphanum",
+	}: "New password must contain letters and numbers only",
+	{
+		"NewPassword",
+		"min",
+	}: "NewPassword must contain at least 5 characters",
+	{
+		"ConfirmNewPassword",
+		"eqfield",
+	}: "New passwords do not match",
 }
